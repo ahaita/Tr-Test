6467d57345e649657de44ede0bb229a9fc1188c3
amina <haita.amina@gmail.com>
2020-06-18 11:38:53 +0100

    Initial repository

---
+++ src/Projet1.java
@@ -0,0 +1,19 @@
+
+public class Projet1 {
+int k=5;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Projet1 hello =new Projet1();
+		for(int I=0; I<hello.k; I++)
+	      hello.SayHello();
+	}
+		public void SayHello()
+		{
+			System.out.println("Hello Java World !!!");
+		}
+    
+    
+	}
+
+
